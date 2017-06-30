c29b8490f79d6a244b44f06c8d7ecaa489a5da4d
taodaren <taodaren1125@gmail.com>
2017-06-30 18:26:48 +0800

    设置底部导航栏

--- app/src/main/java/com/atmhls/givejoy/MainActivity.java
+++ app/src/main/java/com/atmhls/givejoy/MainActivity.java
@@ -1,7 +1,10 @@
 package com.atmhls.givejoy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.majiajie.pagerbottomtabstrip.NavigationController;
+import me.majiajie.pagerbottomtabstrip.PageBottomTabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setBottomTab();
+    }
+
+    private void setBottomTab() {
+        PageBottomTabLayout tab = (PageBottomTabLayout) findViewById(R.id.tab);
+        NavigationController navController = tab.material()
+                .addItem(R.mipmap.tab_home, "首页")
+                .addItem(R.mipmap.tab_category, "附近店铺")
+                .addItem(R.mipmap.tab_enter, "邀请入驻")
+                .addItem(R.mipmap.tab_mine, "我的")
+                .build();
     }
 }
