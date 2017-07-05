256b99431d8834640df64716cd6825e4f2c19a84
taodaren <taodaren1125@gmail.com>
2017-07-05 20:22:22 +0800

    为 「附近店铺」与「邀请入驻」模块设置 WebView 壳

--- app/src/main/java/com/atmhls/category/CategoryFragment.java
+++ app/src/main/java/com/atmhls/category/CategoryFragment.java
@@ -1,6 +1,8 @@
 package com.atmhls.category;
 
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.atmhls.givejoy.R;
 import com.atmhls.global.BaseFragment;
@@ -14,7 +16,15 @@ public class CategoryFragment extends BaseFragment {
     @Override
     public View initView() {
         View inflate = View.inflate(getContext(), R.layout.fragment_category, null);
+        setWebView(inflate);
         return inflate;
+    }
+
+    private void setWebView(View inflate) {
+        WebView webView = (WebView) inflate.findViewById(R.id.web_category);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://stormzhang.com/posts/");
     }
 
 }
--- app/src/main/java/com/atmhls/enter/EnterFragment.java
+++ app/src/main/java/com/atmhls/enter/EnterFragment.java
@@ -1,6 +1,8 @@
 package com.atmhls.enter;
 
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.atmhls.givejoy.R;
 import com.atmhls.global.BaseFragment;
@@ -14,7 +16,15 @@ public class EnterFragment extends BaseFragment {
     @Override
     public View initView() {
         View inflate = View.inflate(getContext(), R.layout.fragment_enter, null);
+        setWebView(inflate);
         return inflate;
+    }
+
+    private void setWebView(View inflate) {
+        WebView webView = (WebView) inflate.findViewById(R.id.web_enter);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.baidu.com/");
     }
 
 }
