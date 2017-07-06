82b448002536a85ded064d06699904ea7d0fe485
taodaren <taodaren1125@gmail.com>
2017-07-06 21:06:37 +0800

    添加 Menu 用于显示右侧控件（存在不显示 bug）

--- app/src/main/java/com/atmhls/global/BaseFragment.java
+++ app/src/main/java/com/atmhls/global/BaseFragment.java
@@ -1,9 +1,13 @@
 package com.atmhls.global;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +26,8 @@ public abstract class BaseFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext = getActivity();
+
+        setHasOptionsMenu(true);
     }
 
     @Nullable
@@ -47,5 +53,17 @@ public abstract class BaseFragment extends Fragment {
     public void initData() {
     }
 
+    public Toolbar setToolbar(int toolbarId) {
+        Activity activity = getActivity();
+        AppCompatActivity appCompatActivity = (AppCompatActivity) activity;
+        Toolbar toolbar = (Toolbar) appCompatActivity.findViewById(toolbarId);
+        appCompatActivity.setSupportActionBar(toolbar);
+        ActionBar actionBar = appCompatActivity.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+        return toolbar;
+    }
 
 }
--- app/src/main/java/com/atmhls/home/HomeFragment.java
+++ app/src/main/java/com/atmhls/home/HomeFragment.java
@@ -1,6 +1,11 @@
 package com.atmhls.home;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.atmhls.givejoy.R;
 import com.atmhls.global.BaseFragment;
@@ -14,7 +19,38 @@ public class HomeFragment extends BaseFragment {
     @Override
     public View initView() {
         View inflate = View.inflate(getContext(), R.layout.fragment_home, null);
+        setHasOptionsMenu(true);
+
+        initTitle(inflate);
         return inflate;
+    }
+
+    private void initTitle(View inflate) {
+        setToolbar(R.id.toolbar_home);
+        //设置标题
+        TextView textTitle = (TextView) inflate.findViewById(R.id.title_toolbar);
+        textTitle.setVisibility(View.VISIBLE);
+        textTitle.setText(R.string.home_name);
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.toolbar, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.qr_code:
+                Toast.makeText(mContext, "1245", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.five_star:
+                Toast.makeText(mContext, "124", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 
 }
