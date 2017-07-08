8436cb6a2cd0e1d3dc7bc7bba1d96adf7f5cf00d
taodaren <taodaren1125@gmail.com>
2017-07-08 16:25:59 +0800

    完善资料设置标题栏

--- app/src/main/java/com/atmhls/mine/MaterialActivity.java
+++ app/src/main/java/com/atmhls/mine/MaterialActivity.java
@@ -1,7 +1,11 @@
 package com.atmhls.mine;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 import com.atmhls.givejoy.R;
 
@@ -11,5 +15,24 @@ public class MaterialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material);
+        setToolbar();
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_material);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            //设置左上角后退按钮
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            //隐藏 Toolbar 自带标题栏
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+
+        //设置标题
+        TextView textTitle = (TextView) findViewById(R.id.title_toolbar);
+        textTitle.setVisibility(View.VISIBLE);
+        textTitle.setText("资料设置");
     }
 }
