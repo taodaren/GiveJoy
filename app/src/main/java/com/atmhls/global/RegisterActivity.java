6d9e54b16529f0dbdbf0335ff16070e6a3e9c945
taodaren <taodaren1125@gmail.com>
2017-07-10 08:16:25 +0800

    注册逻辑（未完成）

--- app/src/main/java/com/atmhls/global/RegisterActivity.java
+++ app/src/main/java/com/atmhls/global/RegisterActivity.java
@@ -1,11 +1,14 @@
 package com.atmhls.global;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.atmhls.givejoy.R;
 
@@ -16,7 +19,30 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         setToolbar();
+        initView();
+//        setClickListener();
     }
+
+    private void initView() {
+
+    }
+
+//    private void setClickListener() {
+//        _signupButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                signup();
+//            }
+//        });
+//
+//        _loginLink.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //完成注册屏幕并返回到Login活动
+//                finish();
+//            }
+//        });
+//    }
 
     private void setToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_register);
@@ -36,4 +62,89 @@ public class RegisterActivity extends AppCompatActivity {
         textTitle.setText("注册");
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+
+//    public void signup() {
+////        Log.d(TAG, "Signup");
+//
+//        if (!validate()) {
+//            onSignupFailed();
+//            return;
+//        }
+//
+//        _signupButton.setEnabled(false);
+//
+//        final ProgressDialog progressDialog = new ProgressDialog(RegisterActivity.this);
+//        progressDialog.setIndeterminate(true);
+//        progressDialog.setMessage("创建帐户...");
+//        progressDialog.show();
+//
+//        String name = _nameText.getText().toString();
+//        String email = _emailText.getText().toString();
+//        String password = _passwordText.getText().toString();
+//
+//        // TODO: Implement your own signup logic here.
+//
+//        new android.os.Handler().postDelayed(
+//                new Runnable() {
+//                    public void run() {
+//                        //完全调用onSignupSuccess或onSignupFailed
+//                        //这里取决于成功
+//                        onSignupSuccess();
+//                        // onSignupFailed();
+//                        progressDialog.dismiss();
+//                    }
+//                }, 3000);
+//    }
+
+//    public void onSignupSuccess() {
+//        _signupButton.setEnabled(true);
+//        setResult(RESULT_OK, null);
+//        finish();
+//    }
+//
+//    public void onSignupFailed() {
+//        Toast.makeText(getBaseContext(), "登录失败", Toast.LENGTH_LONG).show();
+//
+//        _signupButton.setEnabled(true);
+//    }
+//
+//    public boolean validate() {
+//        boolean valid = true;
+//
+//        String name = _nameText.getText().toString();
+//        String email = _emailText.getText().toString();
+//        String password = _passwordText.getText().toString();
+//
+//        if (name.isEmpty() || name.length() < 3) {
+//            _nameText.setError("至少3个字符");
+//            valid = false;
+//        } else {
+//            _nameText.setError(null);
+//        }
+//
+//        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+//            _emailText.setError("输入一个有效的电子邮件地址");
+//            valid = false;
+//        } else {
+//            _emailText.setError(null);
+//        }
+//
+//        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+//            _passwordText.setError("4到10个字母数字字符\n");
+//            valid = false;
+//        } else {
+//            _passwordText.setError(null);
+//        }
+//
+//        return valid;
+//    }
 }
