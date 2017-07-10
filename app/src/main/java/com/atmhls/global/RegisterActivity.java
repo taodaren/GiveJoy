3e8a36e548f5b2552b82431d85ebd892af7f48f9
taodaren <taodaren1125@gmail.com>
2017-07-10 11:23:03 +0800

    实现注册逻辑

--- app/src/main/java/com/atmhls/global/RegisterActivity.java
+++ app/src/main/java/com/atmhls/global/RegisterActivity.java
@@ -7,12 +7,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.atmhls.givejoy.R;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity {
+    private EditText editPhone, editInviteCode, editPhoneCode;
+    private Button btnRegister, btnCancel, btnGetCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,29 +27,43 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         setToolbar();
         initView();
-//        setClickListener();
+        setClickListener();
     }
 
     private void initView() {
-
+        editPhone = (EditText) findViewById(R.id.edit_phone);
+        editInviteCode = (EditText) findViewById(R.id.edit_invite_code);
+        editPhoneCode = (EditText) findViewById(R.id.edit_phone_code);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+        btnCancel = (Button) findViewById(R.id.btn_cancel_register);
+        btnGetCode = (Button) findViewById(R.id.btn_get_code);
     }
 
-//    private void setClickListener() {
-//        _signupButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                signup();
-//            }
-//        });
-//
-//        _loginLink.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //完成注册屏幕并返回到Login活动
-//                finish();
-//            }
-//        });
-//    }
+    private void setClickListener() {
+        //注册
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+
+        //取消
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //获取验证码
+        btnGetCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 
     private void setToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_register);
@@ -52,6 +73,8 @@ public class RegisterActivity extends AppCompatActivity {
         if (actionBar != null) {
             //设置左上角后退按钮
             actionBar.setDisplayHomeAsUpEnabled(true);
+            //设置导航按钮图标
+            actionBar.setHomeAsUpIndicator(R.drawable.back_title);
             //隐藏 Toolbar 自带标题栏
             actionBar.setDisplayShowTitleEnabled(false);
         }
@@ -72,79 +95,92 @@ public class RegisterActivity extends AppCompatActivity {
         return true;
     }
 
-//    public void signup() {
-////        Log.d(TAG, "Signup");
-//
-//        if (!validate()) {
-//            onSignupFailed();
-//            return;
-//        }
-//
-//        _signupButton.setEnabled(false);
-//
-//        final ProgressDialog progressDialog = new ProgressDialog(RegisterActivity.this);
-//        progressDialog.setIndeterminate(true);
-//        progressDialog.setMessage("创建帐户...");
-//        progressDialog.show();
-//
-//        String name = _nameText.getText().toString();
-//        String email = _emailText.getText().toString();
-//        String password = _passwordText.getText().toString();
-//
-//        // TODO: Implement your own signup logic here.
-//
-//        new android.os.Handler().postDelayed(
-//                new Runnable() {
-//                    public void run() {
-//                        //完全调用onSignupSuccess或onSignupFailed
-//                        //这里取决于成功
-//                        onSignupSuccess();
-//                        // onSignupFailed();
-//                        progressDialog.dismiss();
-//                    }
-//                }, 3000);
-//    }
+    public void register() {
+//        Log.d(TAG, "Signup");
 
-//    public void onSignupSuccess() {
-//        _signupButton.setEnabled(true);
-//        setResult(RESULT_OK, null);
-//        finish();
-//    }
-//
-//    public void onSignupFailed() {
-//        Toast.makeText(getBaseContext(), "登录失败", Toast.LENGTH_LONG).show();
-//
-//        _signupButton.setEnabled(true);
-//    }
-//
-//    public boolean validate() {
-//        boolean valid = true;
-//
-//        String name = _nameText.getText().toString();
-//        String email = _emailText.getText().toString();
-//        String password = _passwordText.getText().toString();
-//
-//        if (name.isEmpty() || name.length() < 3) {
-//            _nameText.setError("至少3个字符");
+        if (!validate()) {
+            onRegisterFailed();
+            return;
+        }
+
+        btnRegister.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(RegisterActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("创建帐户...");
+        progressDialog.show();
+
+        String phone = editPhone.getText().toString();
+        String inviteCode = editInviteCode.getText().toString();
+        String phoneCode = editPhoneCode.getText().toString();
+
+        // TODO: 在这里实现你自己的注册逻辑
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        //完全调用 onRegisterSuccess 或 onRegisterFailed
+                        //这里取决于成功
+                        onRegisterSuccess();
+                        // onRegisterFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    public void onRegisterSuccess() {
+        btnRegister.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onRegisterFailed() {
+        Toast.makeText(getBaseContext(), "登录失败", Toast.LENGTH_LONG).show();
+
+        btnRegister.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String phone = editPhone.getText().toString();
+//        String inviteCode = editInviteCode.getText().toString();
+        String phoneCode = editPhoneCode.getText().toString();
+
+        //判断手机号
+        if (phone.isEmpty() || !(phone.length() == 11) || !(isMobileNO(phone))) {
+            editPhone.setError("请输入一个有效的手机号");
+            valid = false;
+        } else {
+            editPhone.setError(null);
+        }
+
+        //判断邀请码（可以不用邀请码登陆）
+//        if (inviteCode.isEmpty() || !(inviteCode.length() == 6)) {
+//            editInviteCode.setError("请输入6位邀请码");
 //            valid = false;
 //        } else {
-//            _nameText.setError(null);
+//            editInviteCode.setError(null);
 //        }
-//
-//        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-//            _emailText.setError("输入一个有效的电子邮件地址");
-//            valid = false;
-//        } else {
-//            _emailText.setError(null);
-//        }
-//
-//        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-//            _passwordText.setError("4到10个字母数字字符\n");
-//            valid = false;
-//        } else {
-//            _passwordText.setError(null);
-//        }
-//
-//        return valid;
-//    }
+
+        //判断验证码
+        if (phoneCode.isEmpty() || !(phoneCode.length() == 6)) {
+            editPhoneCode.setError("请输入6位手机验证码");
+            valid = false;
+        } else {
+            editPhoneCode.setError(null);
+        }
+
+        return valid;
+    }
+
+    //验证手机号是否为正确手机号
+    public static boolean isMobileNO(String mobiles) {
+
+        Pattern p = Pattern
+                .compile("^(0|86|17951)?(13[0-9]|15[0-9]|17[0-9]|18[0-9]|14[0-9])[0-9]{8}$");
+        Matcher m = p.matcher(mobiles);
+
+        return m.matches();
+    }
 }
