50f6aaa491fe854cb8573c8be18b5a4789f9f3bb
taodaren <taodaren1125@gmail.com>
2017-07-15 14:37:14 +0800

    设置城市选择器

--- app/src/main/java/com/atmhls/global/BaseFragment.java
+++ app/src/main/java/com/atmhls/global/BaseFragment.java
@@ -1,6 +1,7 @@
 package com.atmhls.global;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,8 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.atmhls.givejoy.R;
+import com.lljjcoder.citypickerview.widget.CityPicker;
 
 /**
  * Fragment 基类
@@ -78,6 +81,53 @@ public abstract class BaseFragment extends Fragment {
         //城市选择
         RelativeLayout cityLayout = (RelativeLayout) getActivity().findViewById(R.id.layout_city);
         cityLayout.setVisibility(cityVisibility);
+        cityLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CityPicker cityPicker = new CityPicker.Builder(getContext()).textSize(20)
+                        .title("地址选择")
+                        .backgroundPop(0xa0000000)
+                        .titleBackgroundColor("#f72b36")
+                        .titleTextColor("#ffffff")
+                        .confirTextColor("#ffffff")
+                        .cancelTextColor("#ffffff")
+                        .province("北京市")
+                        .city("北京市")
+                        .district("昌平区")
+                        .textColor(Color.parseColor("#000000"))
+                        .provinceCyclic(true)
+                        .cityCyclic(false)
+                        .districtCyclic(false)
+                        .visibleItemsCount(7)
+                        .itemPadding(10)
+                        .onlyShowProvinceAndCity(false)
+                        .build();
+                cityPicker.show();
+
+                //监听方法，获取选择结果
+                cityPicker.setOnCityItemClickListener(new CityPicker.OnCityItemClickListener() {
+                    @Override
+                    public void onSelected(String... citySelected) {
+                        //省份
+                        String province = citySelected[0];
+                        //城市
+                        String city = citySelected[1];
+                        //区县（如果设定了两级联动，那么该项返回空）
+                        String district = citySelected[2];
+                        //邮编
+                        String code = citySelected[3];
+
+                        TextView textCity = (TextView) getActivity().findViewById(R.id.text_city);
+                        textCity.setText(district);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(getContext(), "已取消", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
         ActionBar actionBar = appCompatActivity.getSupportActionBar();
         if (actionBar != null) {
